76cb48a1e098f6cba8e47ce23b66c89f738662f8
gregelton44 <>
2021-02-05 16:33:56 -0500

    Modified Cards class

--- test/src/Card.java
+++ test/src/Card.java
@@ -2,7 +2,8 @@ public class Card {
     public static char[] suits = {'C', 'D', 'H', 'S'};
     public static String[] values = {"0", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-
-
+    public static void main(String[] args) {
+        System.out.println("Testing Cards class with correct account.");
+    }
 
 }
