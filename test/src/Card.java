eacccfbb91354b115f5c96a2f58ae4bec38360cc
gregelton44 <>
2021-02-05 16:42:53 -0500

    Modified Cards class

--- test/src/Card.java
+++ test/src/Card.java
@@ -1,9 +1,34 @@
 public class Card {
     public static char[] suits = {'C', 'D', 'H', 'S'};
-    public static String[] values = {"0", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public static String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+    public int suit, value;
+
+    public Card(){
+        this.suit = 0;
+        this.value =0;
+    }
+
+    public Card(int s, int v){
+        this.suit = s;
+        this.value = v;
+
+    }
+
+    public String toString(){
+        return suits[suit] + values[value];
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Testing Cards class with correct account.");
+        Card c = new Card();
+        Card c1 = new Card(1,2);
+        System.out.println(c.toString());
+        System.out.println(c1.toString());
     }
+
+
+
+
 
 }
