355641fb3056f6e25a8cab11763d47edec802285
greggoryelton <55141543+greggoryelton@users.noreply.github.com>
2021-02-05 16:28:01 -0500

    Added Cards class

---
+++ test/src/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+    public static char[] suits = {'C', 'D', 'H', 'S'};
+    public static String[] values = {"0", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+    public static void main(String[] args) {
+        System.out.println(suits[0] + values[3]);
+    }
+
+
+
+}
