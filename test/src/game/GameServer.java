d990c1bfdf708099f155a4203e15797cd93bb3b0
gregelton44 <>
2021-02-08 18:22:07 -0500

    Added GameServer and Player files

--- test/src/game/GameServer.java
+++ test/src/game/GameServer.java
@@ -1,5 +1,49 @@
 package game;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+
 public class GameServer {
 
+    public class Server implements Runnable {
+        private Socket socket;
+        private ObjectInputStream dIn;
+        private ObjectOutputStream dOut;
+        private int playerId;
+
+        public Server(Socket s, int pID) {
+            socket = s;
+            playerId = pID;
+            try {
+                dOut = new ObjectOutputStream(socket.getOutputStream());
+                dIn = new ObjectInputStream(socket.getInputStream());
+            } catch (IOException ex) {
+                System.out.println("Server Connection failed");
+            }
+        }
+
+
+        public void run() {
+            try {
+                while (true){
+
+                }
+            } catch (Exception e){
+                System.out.println("Run Failed");
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
