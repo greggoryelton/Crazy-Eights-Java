c664340ed02616e4ac5867f1832e58271394561e
gregelton44 <>
2021-02-08 17:58:48 -0500

    Modified Cards class

--- test/src/Github_Test.java
+++
@@ -1,6 +0,0 @@
-public class Github_Test {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
-
-    }
-}
--- test/src/game/Card.java
+++ test/src/game/Card.java
@@ -1,3 +1,5 @@
+package game;
+
 public class Card {
     public static char[] suits = {'C', 'D', 'H', 'S'};
     public static String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
