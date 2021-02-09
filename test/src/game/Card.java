3684fc7afea38198e5146dfc243bfe463b8408b1
gregelton44 <>
2021-02-09 12:18:22 -0500

    Added deck logic to Card file

--- test/src/game/Card.java
+++ test/src/game/Card.java
@@ -1,10 +1,11 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Card {
     public static char[] suits = {'C', 'D', 'H', 'S'};
-    public static String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public static String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     public int suit, value;
 
     public Card(){
@@ -17,19 +18,71 @@ public class Card {
         this.value = v;
     }
 
+    public static ArrayList<Card> rigDeck(){
+        int size = 52;
+
+        ArrayList<Card> riggedDeck = new ArrayList<>();
+       // ArrayList<Card> d2 = new ArrayList<>();
+
+        /*
+        This part rigs the deck so that the pickup pile is exactly the same as the test cases for Part 1 Testing
+         */
+
+        //6C
+        riggedDeck.add(new Card(0, 5));
+        //6D
+        riggedDeck.add(new Card(1,5));
+        //5C
+        riggedDeck.add(new Card(0,4));
+        //6D
+        riggedDeck.add(new Card(1,5));
+        //5S
+        riggedDeck.add(new Card(3,4));
+        //7H
+        riggedDeck.add(new Card(2,6));
+        //6D
+        riggedDeck.add(new Card(1,5));
+        //5S
+        riggedDeck.add(new Card(3,4));
+        //4H
+        riggedDeck.add(new Card(2,3));
+        //6D
+        riggedDeck.add(new Card(1,5));
+        //8H
+        riggedDeck.add(new Card(2,7));
+        //6C
+        riggedDeck.add(new Card(0, 5));
+
+        return riggedDeck;
+    }
+
     public static ArrayList<Card> deck(){
         int size = 52;
+        ArrayList<Card> deck = new ArrayList<>(size);
+        for(int i=0;i<suits.length;i++){
+            for( int j=0;j<values.length;j++){
+                deck.add(new Card(i,j));
+            }
+        }
+        Collections.shuffle(deck);
+
+        return deck;
+
 
     }
 
     public String toString(){
-        return suits[suit] + values[value];
+        return values[value] + suits[suit];
     }
 
     public static void main(String[] args) {
         Card c = new Card();
         Card c1 = new Card(1,2);
-        System.out.println(c.toString());
-        System.out.println(c1.toString());
+
+        ArrayList<Card> d = deck();
+        System.out.println(d.size());
+        for (Card card : d) {
+           System.out.println(card.toString());
+        }
     }
 }
--- test/src/game/Player.java
+++ test/src/game/Player.java
@@ -15,66 +15,16 @@ public class Player {
 
     private int[] scoresheet = new int[50];
 
-    public int[] playRound(int[] dieRoll) {
-        Scanner myObj = new Scanner(System.in);
-        int count = 0; // reroll 3 times
-        int stop = 0;
+    public Card[] playRound(Card[] hand) {
 
-        //game.printDieRoll(dieRoll);
-        while (stop == 0) {
-            System.out.println("Select an action: ");
-            if (count < 3) {
-                System.out.println("(1) Choose dice number to roll again");
-                System.out.println("(2) Roll all again");
-            }
-            System.out.println("(3) Score this round");
 
-            int act = myObj.nextInt();
-            if (act == 1 && count < 3) {
-                System.out.println("Select the die to hold (Ones not held get rerolled): (1,2...) ");
-                String[] die = (myObj.next()).replaceAll("\\s", "").split(",");
 
-                //dieRoll = game.reRollNotHeld(dieRoll, die);
-                System.out.println("New Roll: ");
-                //game.printDieRoll(dieRoll);
-            }
 
-            if (act == 2 && count < 3) {
-                for (int i = 0; i < dieRoll.length; i++) {
-                    //dieRoll = game.rerollDice(dieRoll, i);
 
-                }
-                System.out.println("New Roll: ");
-                //game.printDieRoll(dieRoll);
-            }
-            count++;
-            if (act == 3) {
-//				set yahtzee bonus if applicable
-                //setScoreSheet(13, game.yahtzeeBonus(scoreSheet, dieRoll));
 
-//				get the score for the option requested
-//				check if its been stored already before adding else ask for another number
-                int r = 0;
-                while (r != -1) {
-                    System.out.println("Where do you want to score this round? (1/2/3...)");
-                    r = myObj.nextInt();
-//					add the yahtzee bonus if the roll was yahtzee and yahtzee is full
-                    /*
-                    if (game.isScoreSheetPositionEmpty(scoreSheet, r)) {
-                        setScoreSheet(scoreRound(r, dieRoll));
-                        r = -1;
-                    } else {
-                        System.out.println("The position is filled. Try another number");
-                    }
-                    */
 
-                }
-                stop = 1;
-            }
-        }
-        //return this.scoreSheet;
-        return this.scoresheet;
 
+        return hand;
     }
 
 
